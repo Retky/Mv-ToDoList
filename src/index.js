bfc939ff004ecbd4dc7c6c5c7f4862ca7577e876
Retky <jose.arredondo.a@outlook.com>
2022-03-23 07:57:21 -0600

    Add 'title & add' elements

--- src/index.js
+++ src/index.js
@@ -22,11 +22,12 @@ const list = [
 
 const newLi = (name, index, compleated) => {
   const element = document.createElement('li');
+  element.classList = 'task'
   if (compleated) {
     element.innerHTML = `<input type="checkbox" name="${index}" checked><label for="${index}">${name}</label>`;
     return element;
   }
-  element.innerHTML = `<input type="checkbox" name="${index}"><label for="${index}">${name}</label>`;
+  element.innerHTML = `<input class="task" type="checkbox" name="${index}"><label for="${index}">${name}</label>`;
   return element;
 };
 
