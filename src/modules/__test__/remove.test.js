de769559c277045eea34521d596a191b91a6e974
mrEckendonk <mike.e@vecks.com.br>
2022-03-30 14:38:10 -0300

    remove jest working 90%

--- src/modules/__test__/remove.test.js
+++ src/modules/__test__/remove.test.js
@@ -3,3 +3,18 @@
  */
 
 import remove from '../remove'
+
+describe('remove Function', () => {
+    test('remove element', () => {
+        document.body.innerHTML = '<ul id="todoList"><li class="task"><input type="checkbox" name="0"><label for="0">Feed cats</label><div class="remove"><img src="img"></div></li></ul>';
+        const taskList = [{'name': 'Feed cats', 'index': 0, 'done': false}];
+        const item = document.querySelector('li');
+        const button = document.querySelector('.remove');
+              
+        const todoList = document.querySelector('#todoList')
+        remove(item, 0, taskList, todoList)
+        button.click()
+        const list = document.querySelectorAll('#todoList li');
+        expect(list).toHaveLength(0);
+    });
+});
