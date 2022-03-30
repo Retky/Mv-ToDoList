06f03ac7d933cd091a03ad76982a005d069e533e
Retky <jose.arredondo.a@outlook.com>
2022-03-30 10:53:30 -0600

    Modify 'addTask' jTest syntax
    For better description

--- src/modules/__test__/addtask.test.js
+++ src/modules/__test__/addtask.test.js
@@ -4,15 +4,14 @@
 
 import {addToLi} from '../addtask'
 
-describe('add to UL', () => {
-  test('add', () => {
+describe('addToLi Function', () => {
+  test('add Li element', () => {
     document.body.innerHTML = '<ul id="todoList"></li>';
 
     const todoList = document.querySelector('#todoList')
 
     addToLi("name", 1, "trashImg", "taskList", todoList)
     const list = document.querySelectorAll('#todoList li');
-
      expect(list).toHaveLength(1);
   });
 });
