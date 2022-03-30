e2421a8263ea878a21673a1603c87e87165997b3
Retky <jose.arredondo.a@outlook.com>
2022-03-30 13:48:38 -0600

    Modify 'add-remove' mock HTML tags to close correctly

--- src/modules/__test__/add-remove.test.js
+++ src/modules/__test__/add-remove.test.js
@@ -9,11 +9,12 @@ import remove from '../remove.mjs';
 
 describe('addToLi Function', () => {
   test('add Li element', () => {
-    document.body.innerHTML = '<ul id="todoList"></li>';
+    document.body.innerHTML = '<ul id="todoList"></ul>';
 
     const todoList = document.querySelector('#todoList');
 
     addToLi('name', 1, 'trashImg', 'taskList', todoList);
+
     const list = document.querySelectorAll('#todoList li');
     expect(list).toHaveLength(1);
   });
@@ -21,7 +22,11 @@ describe('addToLi Function', () => {
 
 describe('listen Function', () => {
   test('Click add Task', () => {
-    document.body.innerHTML = '<form id="add" class="task"><input id="addTask" type="text" name="addTask" value="Feed dogs" placeholder="Add to your list..."><button type="submit" id="enter"></button></form><ul id="todoList"></li>';
+    document.body.innerHTML = `<form id="add" class="task">
+    <input id="addTask" type="text" name="addTask" value="Feed dogs" placeholder="Add to your list...">
+    <button type="submit" id="enter"></button>
+    </form>
+    <ul id="todoList"></ul>`;
 
     const todoList = document.querySelector('#todoList');
     const taskList = [];
@@ -39,14 +44,22 @@ describe('listen Function', () => {
 
 describe('remove Function', () => {
   test('remove element', () => {
-    document.body.innerHTML = '<ul id="todoList"><li class="task"><input type="checkbox" name="0"><label for="0">Feed cats</label><div class="remove"><img src="img"></div></li></ul>';
+    document.body.innerHTML = `<ul id="todoList">
+    <li class="task">
+    <input type="checkbox" name="0">
+    <label for="0">Feed cats</label>
+    <div class="remove"><img src="img"></div>
+    </li>
+    </ul>`;
+
     const taskList = [{ name: 'Feed cats', index: 0, done: false }];
     const item = document.querySelector('li');
     const button = document.querySelector('.remove');
-
     const todoList = document.querySelector('#todoList');
+
     remove(item, 0, taskList, todoList);
     button.click();
+
     const list = document.querySelectorAll('#todoList li');
     expect(list).toHaveLength(0);
   });
