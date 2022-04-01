9d644a9604d0744d75981c070a412cb8a6aea50c
Retky <jose.arredondo.a@outlook.com>
2022-04-01 15:45:26 -0600

    Add 'jsdom' load jTest
    To use for next jTests

---
+++ src/modules/__test__/jsdomLoad.test.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+beforeAll(() => {
+  document.body.innerHTML = fs.readFileSync('src/jsdom/someTasks.html');
+});
+
+describe('test DOM', () => {
+  test('has a 4 list elements', () => {
+    const list = document.querySelectorAll('li');
+    console.log(list.length);
+    expect(list).toHaveLength(4)
+  });
+});
